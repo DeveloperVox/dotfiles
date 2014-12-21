bd6e302359c3e056917dfb507c029223403dc389
Raam Dev <raam@raamdev.com>
2014-12-20 23:35:14 -0500

    Add Homebrew setup for Apache, PHP, MySQL

--- sh-bootstrap/brew.sh
+++ sh-bootstrap/brew.sh
@@ -64,5 +64,61 @@ brew install mu
 brew install isync
 brew install ledger
 
+# ----------------------------------------
+# Install Homebrew
+xcode-select --install
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+ 
+# Tap Repos
+brew tap homebrew/dupes
+brew tap homebrew/versions
+brew tap homebrew/homebrew-php
+brew tap homebrew/apache
+ 
+# Verify
+brew update && brew upgrade
+ 
+# Macintosh
+brew install git
+brew install openssl
+ssh-keygen -t rsa -C “email@address.invalid”
+ssh-add ~/.ssh/id_rsa
+ 
+# Apache
+sudo apachectl stop
+sudo launchctl unload -w /System/Library/LaunchDaemons/org.apache.httpd.plist 2>/dev/null
+brew install httpd24 --with-privileged-ports --with-brewed-ssl
+sudo cp -v /usr/local/Cellar/httpd24/2.4.10/homebrew.mxcl.httpd24.plist /Library/LaunchDaemons
+sudo chown -v root:wheel /Library/LaunchDaemons/homebrew.mxcl.httpd24.plist
+sudo chmod -v 644 /Library/LaunchDaemons/homebrew.mxcl.httpd24.plist
+sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.httpd24.plist
+sudo httpd -k start
+ 
+# MySQL
+brew install mysql
+ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+mysqladmin -u root password “NEWPASSWORD”
+ 
+# PHP
+brew install php56 --homebrew-apxs --with-apache --with-homebrew-curl --with-homebrew-openssl --with-phpdbg --with-tidy --without-snmp
+chmod -R ug+w /usr/local/Cellar/php56/5.6.2/lib/php
+pear config-set php_ini /usr/local/etc/php/5.6/php.ini
+printf '\nAddHandler php5-script .php\nAddType text/html .php' >> /usr/local/etc/apache2/2.4/httpd.conf
+perl -p -i -e 's/DirectoryIndex index.html/DirectoryIndex index.php index.html/g' /usr/local/etc/apache2/2.4/httpd.conf
+printf '\nexport PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"' >> ~/.profile
+ 
+# Dev Stuff
+brew install composer
+brew install behat
+brew install node
+npm -g install grunt
+npm -g install shifter
+brew tap danpoltawski/homebrew-mdk
+brew install moodle-sdk
+
+# ---------------------------------
+
 # Remove outdated versions from the cellar.
 brew cleanup
